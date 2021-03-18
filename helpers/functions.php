07c21fab136f6eaba51205c6fd305bf66f6d5fd0
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-03-18 10:55:12 -0500

    se modificaron links2

--- helpers/functions.php
+++ helpers/functions.php
@@ -218,14 +218,14 @@ function precor_remove_my_account_links($menu_links)
 {
 
      // unset($menu_links['edit-address']); // Addresses
-     // unset($menu_links['dashboard']); // Remove Dashboard
+     unset($menu_links['dashboard']); // Remove Dashboard
      unset($menu_links['payment-methods']); // Remove Payment Methods
      unset($menu_links['orders']); // Remove Orders
      unset($menu_links['downloads']); // Disable Downloads
      unset($menu_links['edit-account']); // Remove Account details tab
      unset($menu_links['edit-address']); // Remove Account details tab
      unset($menu_links['customer-logout']); // Remove Logout link
-     $menu_links['dashboard'] = "Mi Cuenta";
+    // $menu_links['dashboard'] = "Mi Cuenta";
 
      $new = array(
           'catalogoProductos' => 'Catalogo de Productos',
