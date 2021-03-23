789d45823ed305029b2029f9325e11280505e7de
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-03-23 09:08:56 -0500

    solo se activara pedidos si yo lo actio

--- woocommerce/checkout/payment.php
+++ woocommerce/checkout/payment.php
@@ -17,7 +17,7 @@
  */
 
 defined('ABSPATH') || exit;
-
+$realizarPedidos = false;
 if (!is_ajax()) {
 	do_action('woocommerce_review_order_before_payment');
 }
@@ -90,10 +90,13 @@ if (!is_ajax()) {
 
 		<?php do_action('woocommerce_review_order_before_submit'); ?>
 
-		<!-- <?php echo apply_filters('woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr($order_button_text) . '" data-value="' . esc_attr($order_button_text) . '">' . 
-		esc_html($order_button_text) . '</button>'); 
-		// @codingStandardsIgnoreLine 
-		?> -->
+		<?php
+		if ($realizarPedidos) {
+			echo apply_filters('woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr($order_button_text) . '" data-value="' . esc_attr($order_button_text) . '">' .
+				esc_html($order_button_text) . '</button>');
+			// @codingStandardsIgnoreLine 
+		}
+		?>
 
 		<?php do_action('woocommerce_review_order_after_submit'); ?>
 
