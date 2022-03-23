e0867aec36e1033f4a50e5d4bf979885cab088cd
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-03-23 16:58:23 -0500

    cuando el estado es completado que aparezca completado en el portal y en las ordenes

--- helpers/functions.php
+++ helpers/functions.php
@@ -720,6 +720,8 @@ function precor_translateStatus($quote, $statusCode = null): string
           $spanish = "pago procesado";
      } else if ($statusCode == 6) {
           $spanish = "pago rechazado";
+     }else if ($status == "completed"){
+          $spanish = "completado";
      }
 
 
